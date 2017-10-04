d4cdc7eeb41082753c277920b01354230793c690
callmezzzhy <32006516+callmezzzhy@users.noreply.github.com>
2017-10-04 10:21:06 +0800

    Update 邮箱.py

--- 12360/邮箱.py
+++ 12360/邮箱.py
@@ -2,22 +2,25 @@ from smtplib import SMTP_SSL
 from email.mime.text import MIMEText
 from email.header import Header
 def post():
-    host_server='smtp.qq.com'
-    sender_qq='470340062'
-    pwd='pyzuyfoqlxrgcbdi'
-    sender_qq_mail='470340062@qq.com'
-    receiver ='657984297@qq.com'
-    mail_content='你好，我是来自知乎的callmezzzhy，现在在进行一项用python登录qq邮箱的练习'
-    mail_title='来自远方的一封信'
+    host_server='smtp.qq.com' 
+    sender_qq='xxxxxxx                           #发送方qq号
+    pwd='pyzuyfoqlxrgcbdi'                       #qq开启第三方smtp 验证码
+    sender_qq_mail='xxxxxxx@qq.com'              #发送方邮箱
+    receiver ='xxxxxxx@qq.com' #接收者邮箱
+    mail_content='你好，你查询的车票信息：xxxx 有票啦，可以赶紧登陆抢票 '           #邮件内容
+    mail_title='来自远方的一封信'                   #邮件标题
     smtp=SMTP_SSL(host_server)
     smtp.set_debuglevel(1)
     smtp.ehlo(host_server)
     smtp.login(sender_qq,pwd)
+    '''
+    配置邮件内容及格式
+    '''
     msg=MIMEText(mail_content,"html",'utf-8')
     msg["Subject"]=Header(mail_title,'utf-8')
     msg["From"]=sender_qq_mail
     msg["To"]=receiver
-    smtp.sendmail(sender_qq_mail,receiver,msg.as_string())
+    smtp.sendmail(sender_qq_mail,receiver,msg.as_string())  
     smtp.quit()
 
 
