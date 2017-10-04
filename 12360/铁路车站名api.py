fb5592ecdf45e627b5333090e2e1eed13e6c827e
callmezzzhy <32006516+callmezzzhy@users.noreply.github.com>
2017-10-04 10:15:25 +0800

    Update 铁路车站名api.py

--- 12360/铁路车站名api.py
+++ 12360/铁路车站名api.py
@@ -1,9 +1,9 @@
 import requests
 import re
 
-requests.packages.urllib3.disable_warnings()
+requests.packages.urllib3.disable_warnings() #禁用安全请求警告
 url='https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.9018'
-r=requests.get(url,verify=False)
+r=requests.get(url,verify=False) #忽略SSL证书验证
 pattern=u'([\u4e00-\u9fa5]+)\|([A-Z]+)'
 result=re.findall(pattern,r.text)
 station=dict(result)
