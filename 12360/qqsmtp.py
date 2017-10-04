717e9bf58f07d1509239b58664eaccb62d98a2e8
callmezzzhy <32006516+callmezzzhy@users.noreply.github.com>
2017-10-04 15:57:34 +0800

    Update and rename 邮箱.py to qqsmtp.py

--- 12360/qqsmtp.py
+++ 12360/qqsmtp.py
@@ -1,13 +1,13 @@
 from smtplib import SMTP_SSL
 from email.mime.text import MIMEText
 from email.header import Header
-def post():
+def post1(data):
     host_server='smtp.qq.com' 
     sender_qq='xxxxxxx                           #发送方qq号
-    pwd='pyzuyfoqlxrgcbdi'                       #qq开启第三方smtp 验证码
+    pwd='xxxxxx'                       #qq开启第三方smtp 验证码
     sender_qq_mail='xxxxxxx@qq.com'              #发送方邮箱
     receiver ='xxxxxxx@qq.com' #接收者邮箱
-    mail_content='你好，你查询的车票信息：xxxx 有票啦，可以赶紧登陆抢票 '           #邮件内容
+    mail_content=('你好，你查询的车票信息：{} 有票啦，可以赶紧登陆抢票 '.format(data))           #邮件内容
     mail_title='来自远方的一封信'                   #邮件标题
     smtp=SMTP_SSL(host_server)
     smtp.set_debuglevel(1)
